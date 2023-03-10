ea8f36ffe1202ad7b37cbd0f1b8b8a3b3c826c96
Maks <darcksday@gmail.com>
2023-03-10 18:20:12 +0200

    update rainbow

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import './assets/styles/index.css'
 // // Bootstrap Bundle JS
 import { Web3Provider } from "./context/Web3Context";
 import { BrowserRouter } from 'react-router-dom';
-import ThemeProvider from './theme';
-import '@rainbow-me/rainbowkit/styles.css';
+import ThemeProvider from './thefme';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
